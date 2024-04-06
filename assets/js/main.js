07befc861a8bf949332433e313d4aa53c32079ff
Nisha Bukke <sononenisha827@gmail.com>
2024-04-06 17:25:50 +0530

    developed inner pages and seo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,43 @@
 /*=============== SHOW MENU ===============*/
 
+const navMenu =  document.getElementById('nav-menu'),
+  navToggle =  document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+
 /*===== Menu Show =====*/
 /* Validate if constant exists */
 
+if(navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
+
 /*===== Hide Show =====*/
 /* Validate if constant exists */
+
+if(navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  })
+}
+
+/*=============== Q AND A ACCORDION ===============*/
+
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight){
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      } 
+    });
+  }
 
 /*=============== IMAGE GALLERY ===============*/
 
@@ -32,22 +65,26 @@ var swiperCategories = new Swiper(".categories__container", {
       prevEl: ".swiper-button-prev",
     },
     breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 24,
-        },
-        1400: {
-          slidesPerView: 6,
-          spaceBetween: 24,
-        },
+      450: {
+        slidesPerView: 1,
+        spaceBetween: 24,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      1200: {
+        slidesPerView: 5,
+        spaceBetween: 24,
+      },
+      1400: {
+        slidesPerView: 6,
+        spaceBetween: 24,
+      },
       },
   });
 
@@ -61,15 +98,19 @@ var swiperProducts = new Swiper(".new__container", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 20,
+      450: {
+        slidesPerView: 1,
+        spaceBetween: 24,
       },
       768: {
-        slidesPerView: 4,
-        spaceBetween: 40,
+        slidesPerView: 3,
+        spaceBetween: 24,
       },
-      1024: {
+      992: {
+        slidesPerView: 4,
+        spaceBetween: 24,
+      },
+      1200: {
         slidesPerView: 5,
         spaceBetween: 24,
       },
@@ -101,3 +142,64 @@ tabContents = document.querySelectorAll('[content]');
       tab.classList.add('active-tab');
     });
   });
+
+
+ /*============== CHANGE SIZE ================*/
+ 
+ var prevClickedElement = null;
+
+ function changeSize(id) {
+     var element = document.getElementById(id);
+     
+     // Reset background color of the previously clicked element
+     if (prevClickedElement !== null) {
+       prevClickedElement.style.backgroundColor = '#f2f2f2';
+       prevClickedElement.style.color = '#222';
+     }
+     
+     // Set background color of the currently clicked element
+     element.style.backgroundColor = '#000';
+     element.style.color = '#fff';
+     
+     // Update the previously clicked element
+     prevClickedElement = element;
+}
+
+
+ /*============== CHANGE COLOR ================*/
+ 
+ var prevClickedColor = null;
+
+ function changeColor(id) {
+     var color = document.getElementById(id);
+
+     console.log(color);
+     
+     // Reset background color of the previously clicked element
+     if (prevClickedColor !== null) {
+       prevClickedColor.style.border = 'none';
+       prevClickedColor.style.padding = 'none';
+     }
+     
+     // Set background color of the currently clicked element
+     color.style.border = '1px solid #000';
+     color.style.padding = '.5rem';
+     
+     // Update the previously clicked element
+     prevClickedColor = color;
+}
+
+
+/*========= EMBROIDARY ======*/
+
+// Get the checkbox element
+var checkbox = document.getElementById("embroideryCheckbox");
+
+// Get the embroidery-div element
+var embroideryDiv = document.querySelector(".embroidery-div");
+
+// Add event listener to the checkbox
+checkbox.addEventListener("change", function() {
+  // Toggle visibility of embroidery-div based on checkbox state
+  embroideryDiv.style.display = this.checked ? "flex" : "none";
+});
